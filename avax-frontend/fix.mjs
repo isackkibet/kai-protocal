416cf91beec3499c478a9d3fab37ac0f0a249b40
isackkibet <isackkibet97@gmail.com>
2026-09-16 01:05:02 +0300

    chore: remove unused Prisma client queries and related code from avax-frontend

--- avax-frontend/fix.mjs
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-await prisma.$executeRawUnsafe(`ALTER TYPE "Chain" ADD VALUE IF NOT EXISTS 'AVALANCHE'`);
-console.log('done: AVALANCHE added to Chain enum');
-await prisma.$disconnect();
--- avax-frontend/q.mjs
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-const r = await prisma.$queryRawUnsafe(`SELECT e.enumlabel FROM pg_type t JOIN pg_enum e ON t.oid = e.enumtypid WHERE t.typname = 'Chain' ORDER BY e.enumsortorder`);
-console.log('Chain enum values:', r.map(x=>x.enumlabel).join(', '));
-await prisma.$disconnect();
